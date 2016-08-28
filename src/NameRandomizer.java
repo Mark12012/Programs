d3a2dfdb002cf86d7fb165789c66109ce00250ca
Marek <Mark12012@gmail.com>
2016-08-28 15:01:41 +0200

    Test

---
+++ src/NameRandomizer.java
@@ -0,0 +1,7 @@
+package com.project;
+
+/**
+ * Created by Marek on 2016-08-27.
+ */
+public class NameRandomizer {
+}
---
+++ src/com/project/NameRandomizer.java
@@ -0,0 +1,23 @@
+package com.project;
+
+import java.io.File;
+
+/**
+ * Created by Marek on 2016-08-27.
+ */
+public class NameRandomizer {
+
+    private String name;
+    private String lastName;
+
+    void startFiles()
+    {
+        File names;
+    }
+
+    void endFiles()
+    {
+
+    }
+
+}
